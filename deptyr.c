3b0fc430a6d33d74dd646f78abe04095f0b01485
Andreas Fuchs <asf@boinkor.net>
2016-04-02 01:33:13 -0700

    Pick stringly ptsname on Linux via _XOPEN_SOURCE
    I know, ugh, but hey - it does work there (-:

--- deptyr.c
+++ deptyr.c
@@ -22,6 +22,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#ifdef __linux__
+#define _XOPEN_SOURCE
+#endif
+
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
