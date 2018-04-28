9041b2061f09a1e7205c3f792c740535dc7863ce
Andreas Fuchs <asf@boinkor.net>
2018-04-28 17:54:23 +0200

    Set up the process-running stuff & PTY allocation
    Likely doesn't work yet. It's close to what deptyr does, though.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 extern crate clap;
 #[macro_use]
 extern crate failure;
+extern crate libc;
+#[macro_use]
 extern crate nix;
 extern crate sendfd;
 
 use std::ffi::CString;
-use std::os::unix::ffi::OsStringExt;
 use std::ffi::OsString;
+use std::io::{stderr, stdin, stdout};
+use std::os::unix::ffi::OsStringExt;
+use std::os::unix::io::{AsRawFd, RawFd};
+use std::path::Path;
+
 use clap::{App, Arg, SubCommand};
-use nix::unistd::execvp;
+
 use failure::Error;
+
+use libc::{TIOCGWINSZ, TIOCSWINSZ};
+
+use nix::unistd::{close, execvp, getppid, setpgid, setsid, Pid, dup2};
+use nix::fcntl::{open, OFlag};
+use nix::pty::{grantpt, posix_openpt, ptsname, unlockpt, Winsize};
+// use nix::sys::termios::{cfmakeraw, Termios};
+use nix::sys::stat::Mode;
 
 fn main() {
     let matches =
@@ -54,18 +68,77 @@ fn interact() -> Result<(), Error> {
     Ok(())
 }
 
-fn run(command: Vec<OsString>) -> Result<(), Error> {
-    // TODO: Send PTY through unix domain socket
+ioctl!{bad write_ptr unsafe_tty_set_winsize with TIOCSWINSZ; Winsize}
+ioctl!{bad read unsafe_tty_get_winsize with TIOCGWINSZ; Winsize}
 
-    // TODO: Redirect IO to PTY
-
-    println!("Running: {:?}", command);
-    unsafe {
-        let cstr_command: Vec<CString> = command
-            .into_iter()
-            .map(|arg| CString::from_vec_unchecked(arg.into_vec()))
-            .collect();
-        try!(execvp(&cstr_command[0], &cstr_command));
-        bail!("continued after execvp - this should never be reached");
+fn default_winsize() -> Winsize {
+    Winsize {
+        ws_row: 80,
+        ws_col: 30,
+        ws_xpixel: 640,
+        ws_ypixel: 480,
     }
+}
+
+fn tty_get_winsize(fd: RawFd) -> Result<Winsize, Error> {
+    let mut ws = default_winsize();
+    unsafe {
+        try!(unsafe_tty_get_winsize(fd, &mut ws));
+    }
+    Ok(ws)
+}
+
+fn tty_set_winsize(fd: RawFd, ws: Winsize) -> Result<Winsize, Error> {
+    unsafe {
+        try!(unsafe_tty_set_winsize(fd, &ws));
+    }
+    Ok(ws)
+}
+
+fn resize_pty(fd: RawFd) -> Result<(), Error> {
+    match tty_get_winsize(stdin().as_raw_fd()) {
+        Err(_) => {
+            // We have no tty on stdin - let's still tell the client
+            // it has a "normal"-sized window:
+            try!(tty_set_winsize(fd, default_winsize()));
+        }
+        Ok(ws) => {
+            try!(tty_set_winsize(fd, ws));
+        }
+    }
+    Ok(())
+}
+
+fn run(command: Vec<OsString>) -> Result<(), Error> {
+    // Open the PTY:
+    let controlling_fd = posix_openpt(OFlag::O_RDWR)?;
+    grantpt(&controlling_fd)?;
+    unlockpt(&controlling_fd)?;
+    let client_pathname = unsafe { ptsname(&controlling_fd) }?; // POSIX calls this the "slave", but no.
+    let client_fd = open(Path::new(&client_pathname), OFlag::O_RDWR, Mode::empty())?;
+    tty_set_winsize(client_fd, default_winsize())?;
+
+    // Make a new session & redirect IO to PTY
+    setpgid(Pid::this(), getppid())?;
+    setsid()?;
+    let newstdin = open(Path::new(&client_pathname), OFlag::O_RDONLY, Mode::empty())?;
+    dup2(newstdin, stdin().as_raw_fd())?;
+    close(newstdin)?;
+
+    let newout = open(Path::new(&client_pathname), OFlag::O_WRONLY, Mode::empty())?;
+    dup2(newout, stdout().as_raw_fd())?;
+    dup2(newout, stderr().as_raw_fd())?;
+    close(newout)?;
+
+    // TODO: Send controlling_fd through UNIX domain socket
+
+    // Run the command:
+    close(controlling_fd.as_raw_fd())?;
+    println!("Running: {:?}", command);
+    let cstr_command: Vec<CString> = command
+        .into_iter()
+        .map(|arg| unsafe { CString::from_vec_unchecked(arg.into_vec()) })
+        .collect();
+    try!(execvp(&cstr_command[0], &cstr_command));
+    bail!("continued after execvp - this should never be reached")
 }
